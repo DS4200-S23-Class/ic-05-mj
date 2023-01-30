8834600f9aaf58148ef99799b4dcc578fe93e7ce
Jane <ytzheng2003@gmail.com>
2023-01-30 16:17:22 -0500

    Added new function

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // button function 
+
+function addOne(num){
+	return num + 1;
+}
+
 function buttonClicked(){
-	let newText = "Number of times button clicked:"
-	let num = 0
+	let newText = "Number of times button clicked:";
+	let num = 0;
 
 	// select element to be modified
 	let buttonDiv = document.getElementById("button-div");
-	buttonDiv.innerHTML = newText + num + 1
+	buttonDiv.innerHTML = newText + addOne(num);
 }
